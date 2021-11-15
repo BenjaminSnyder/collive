9e44c780650ab0720a896d9bc72e233f73cbc15a
bensnyder <bs3148@columbia.edu>
2021-11-15 01:56:58 -0500

    updated style of db and document, added gitignore to ignore vscode

--- src/documents/document.py
+++ src/documents/document.py
@@ -1,37 +1,39 @@
 from database.db import Database
+
+
 class Document():
     def __init__(self, user_id):
         self.database = Database(user_id)
 
-    def loadDocument(self, document_id):
-        dictionary = self.database.getDocument(document_id)
-        self.__dictToAttributes(dictionary)
+    def load_document(self, document_id):
+        dictionary = self.database.get_document(document_id)
+        self.__dict_to_attributes(dictionary)
         return dictionary
 
-    def deleteDocument(self, document_id):
+    def delete_document(self, document_id):
         self.document_id = document_id
-        self.database.deleteDocument(self.__convertToDict())
-        return self.__convertToDict()
+        self.database.delete_document(self.__convert_to_dict())
+        return self.__convert_to_dict()
 
-    def updateContent(self, content):
+    def update_content(self, content):
         if self.document_id is None:
             return "ERROR: Document not Loaded"
         self.revision += 1
         self.content = content
-        self.database.insert(self.__convertToDict())
-        return self.__convertToDict()
+        self.database.insert(self.__convert_to_dict())
+        return self.__convert_to_dict()
 
-    def createDocument(self):
+    def create_document(self):
         self.revision = 0
-        dictionary = self.database.createDocument(self.__convertToDict())
-        self.__dictToAttributes(dictionary)
-        return self.__convertToDict()
+        dictionary = self.database.create_document(self.__convert_to_dict())
+        self.__dict_to_attributes(dictionary)
+        return self.__convert_to_dict()
 
-    def __convertToDict(self):
+    def __convert_to_dict(self):
         return {'document_id':self.document_id, 'name':self.name, 'content':self.content,
                 'revision':self.revision}
 
-    def __dictToAttributes(self, dictionary):
+    def __dict_to_attributes(self, dictionary):
         self.document_id = dictionary["document_id"]
         self.name = dictionary["name"]
         self.content = dictionary["content"]
