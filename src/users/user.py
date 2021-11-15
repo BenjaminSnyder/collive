6a32989a5efe876b4d351fddbc9c6d7b439f91df
jedster99 <dreibelbisjordan@gmail.com>
2021-11-14 23:51:54 -0500

    Updated User class

--- src/users/user.py
+++ src/users/user.py
@@ -3,20 +3,8 @@ class User():
     def __init__(self, user_id):
         self.database = Database("users")
         self.user_id = user_id
-    def loadDocument(self, document_id):
-        dictionary = self.database.getDocument(document_id)
-        self.__dictToAttributes(dictionary)
-        return dictionary
-
-    def deleteDocument(self, document_id):
-        self.document_id = document_id
-        self.database.deleteDocument(self.__convertToDict())
 
     def deleteUser(self, content):
-        if self.document_id is None:
-            return "ERROR: Document not Loaded"
-        self.revision += 1
-        self.content = content
         self.database.insert(self.__convertToDict())
         return "SUCCESS"
 
@@ -24,15 +12,10 @@ class User():
         self.database.createUser(self.__convertToDict())
 
     def __convertToDict(self):
-        return {'document_id':self.document_id, 'name':self.name, 'content':self.content,
-                'users':self.users, 'revision':self.revision}
+        return {'user_id':self.user_id}
 
     def __dictToAttributes(self, dictionary):
-        self.document_id = dictionary["document_id"]
-        self.name = dictionary["name"]
-        self.content = dictionary["content"]
-        self.users = dictionary["users"]
-        self.users = dictionary["users"]
+        self.user_id = dictionary["user_id"]
 
     user_id = None
     database = None
