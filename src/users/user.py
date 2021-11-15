877764b243346b82686b97303a7e1ecf92c6049e
jedster99 <dreibelbisjordan@gmail.com>
2021-11-15 01:12:05 -0500

    Added createUser and deleteUser

--- src/users/user.py
+++ src/users/user.py
@@ -1,21 +1,8 @@
-from database.db import Database
+from tinydb import TinyDB
 class User():
-    def __init__(self, user_id):
-        self.database = Database("users")
-        self.user_id = user_id
-
-    #def deleteUser(self, content):
-    #    self.database.d(self.__convertToDict())
-    #    return "SUCCESS"
-
+    def deleteUser(self, user_id):
+        db = TinyDB(user_id + '/docs.json')
+        db.truncate()
+        
     def createUser(self, user_id):
-        self.database.createUser(self.__convertToDict())
-
-    def __convertToDict(self):
-        return {'user_id':self.user_id}
-
-    def __dictToAttributes(self, dictionary):
-        self.user_id = dictionary["user_id"]
-
-    user_id = None
-    database = None
+        TinyDB(user_id + '/docs.json')
