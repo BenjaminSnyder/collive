ee2c62ebd4353dd609e5978117401a9943c77c35
jedster99 <dreibelbisjordan@gmail.com>
2021-11-16 01:10:58 -0500

    Updated test_document

--- tests/unit/documents/test_document.py
+++ tests/unit/documents/test_document.py
@@ -89,7 +89,9 @@ def test_update_meta():
     rev = doc.revision
     assert dictionary["document_id"] == doc_id
     assert dictionary["revision"] == doc.revision
-    assert dictionary["name"] == "update_doc_txt"
+    assert dictionary["name"] == "newName.txt"
+    assert dictionary["users"][0] == "client"
+    assert dictionary["users"][1] == "client2"
     assert dictionary["viewers"][0] == "client"
     assert dictionary["viewers"][1] == "client2"
 
@@ -102,34 +104,17 @@ def test_update_meta():
     
     assert dictionary["viewers"][0] == "client3"
     assert dictionary["viewers"][1] == "client4"
+    assert dictionary["users"][0] == "client3"
+    assert dictionary["users"][1] == "client4"
 
     response = doc.update_meta("error_doc", "client")
     assert response == "ERROR: Document does not exist"
-    response = doc.delete_document("update_doc.txt")
+    response = doc.delete_document("client3")
 
 def test_create_document():
     doc = Document("test")
-    response = doc.create_document()
-    assert response != "ERROR: Could not crate new document."
-    doc_id = doc.create_document()
-    dictionary = doc.load_document(doc_id)
-    response = doc.update_content("update_doc.txt", "This is a a doc to update")
+    doc_id = doc.create_document("create_doc.txt", "client")
+    assert doc_id != "ERROR: Could not create new document."
+    dictionary = doc.load_document(doc_id, "client")
+    response = doc.update_content("This is a a doc to update", "client")
     assert response == "SUCCESS"
-
-    dictionary = doc.load_document(doc_id)
-
-    assert dictionary["document_id"] == doc_id
-    assert dictionary["revision"] == "1"
-    assert dictionary["name"] == "update_doc_txt"
-    assert dictionary["content"] == "This is a doc to update"
-
-    response = doc.update_content("update_doc.txt", "This is a a doc to update version 2")
-    assert response == "SUCCESS"
-    assert dictionary["document_id"] == doc_id
-    assert dictionary["revision"] == "2"
-    assert dictionary["name"] == "update_doc_txt"
-    assert dictionary["content"] == "This is a doc to update verion 2"
-
-    response = doc.update_content("error_doc")
-    assert response == "ERROR: Document does not exist"
-    
