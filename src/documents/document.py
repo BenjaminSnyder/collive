d2fe50c8e8539a709a3ba13a2a05bd79a0b648db
jedster99 <dreibelbisjordan@gmail.com>
2021-11-16 22:12:39 -0500

    Ensure client_id is str

--- src/documents/document.py
+++ src/documents/document.py
@@ -23,6 +23,7 @@ class Document():
         information retrieved from the database.
         '''
 
+        client_id = str(client_id)
         meta = db.get_meta(self.token, document_id)
         if type(meta) == str:
             return meta
@@ -52,6 +53,7 @@ class Document():
         if the given client is authenticated.
         '''
 
+        client_id = str(client_id)
         if self.document_id is None:
             return "ERROR: Document not loaded."
         if self.__authorize_client(client_id, "u"):
@@ -70,6 +72,7 @@ class Document():
         the database with a client's contents.
         '''
 
+        client_id = str(client_id)
         if self.document_id is None:
             return "ERROR: Document not loaded."
 
@@ -94,6 +97,7 @@ class Document():
         in the database with a client's meta data.
         '''
 
+        client_id = str(client_id)
         if self.document_id is None:
             return "ERROR: Document not loaded."
 
@@ -111,6 +115,8 @@ class Document():
         Creates a new document with a given
         name in the document database.
         '''
+
+        client_id = str(client_id)
         self.name = name
         self.revision_hash = Document_Util.create_hash("")
         self.users = [client_id]
