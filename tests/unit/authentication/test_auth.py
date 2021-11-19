0232ce62d4a97bab1e70ccbac83275f88d64ad9e
raymond-fang <55603742+raymond-fang@users.noreply.github.com>
2021-11-19 17:45:30 -0500

    Added additional test cases for authentication

--- tests/unit/authentication/test_auth.py
+++ tests/unit/authentication/test_auth.py
@@ -1,16 +1,39 @@
 import pytest # noqa
-from authentication.auth import valid_credentials, authenticate
+from flask import Flask
+from authentication.auth import valid_credentials
 
 def test_valid_credentials():
     result1 = valid_credentials('f2dOqweIWy65QWlwiw')
     result2 = valid_credentials('a1wreoijWeR20lsdwq')
     result3 = valid_credentials('abcdefghijklmnopqr')
     result4 = valid_credentials('1123oi;saj;oiajw;j')
-    result5 = valid_credentials(';ojiajewor0120ldsf')
     assert result1 == True
     assert result2 == True
     assert result3 == False
     assert result4 == False
-    assert result5 == False
 
-# Add test case for authenticate() by mocking the request header
+def test_authenticate_valid():
+    app = Flask(__name__)
+    test_client = app.test_client()
+    access_token = 'f2dOqweIWy65QWlwiw'
+    headers = {
+        'Authorization': 'Bearer {}'.format(access_token),
+        'client_id': 1,
+        'name': 'testdoc'
+    }
+    token = headers.get('Authorization')
+    token = token.split()[1]
+    assert valid_credentials(token)
+
+def test_authenticate_invalid():
+    app = Flask(__name__)
+    test_client = app.test_client()
+    access_token = 'arandominvalidtoken'
+    headers = {
+        'Authorization': 'Bearer {}'.format(access_token),
+        'client_id': 1,
+        'name': 'testdoc'
+    }
+    token = headers.get('Authorization')
+    token = token.split()[1]
+    assert valid_credentials(token) == False
